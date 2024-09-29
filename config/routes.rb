b3da4c04401deed58ddfc1ec2a988c4d6fcca717
EC2 Default User <ec2-user@ip-172-31-4-116.ap-northeast-1.compute.internal>
2024-09-29 19:50:38 +0000

    1st dp

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'lists#index'
 
-  resources :lists, only: [:new, :create, :index, :show, :edit, :destroy]
+  resources :lists, only: [:new, :create, :index, :show, :edit, :destroy, :update]
   resources :users, only: [:show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

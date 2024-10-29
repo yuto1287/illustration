ca9c7c15305abfc31720e53b9712f5b0bb0bde3c
EC2 Default User <ec2-user@ip-172-31-4-116.ap-northeast-1.compute.internal>
2024-10-29 12:35:48 +0000

    実装フェーズ2完了

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,13 +1,13 @@
 class Public::SearchesController < ApplicationController
   before_action :authenticate_user!
-  
+
   def search
     if params[:model] == 'user'
       @users = User.search_for(params[:content], params[:method]).order(created_at: :desc)
-      render 'users/index'
+      render 'public/users/index'
     else
       @lists = List.search_for(params[:content], params[:method]).order(created_at: :desc)
-      render 'lists/index'
+      render 'public/lists/index'
     end
-  end 
+  end
 end

18e668a37b1fc28f383c99a62a7d20579e5af35d
EC2 Default User <ec2-user@ip-172-31-4-116.ap-northeast-1.compute.internal>
2024-10-29 13:57:45 +0000

    2ndデプロイ＋テスト完了

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :password], controllers: {sessions: 'admin/sessions'}
 
   namespace :admin do
+    root to: "dashboards#index"
     get 'dashboards', to: 'dashboards#index'
     resources :users, only: [:destroy]
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Admin.create!(email: "yuto@example.com", password: "password")
+
 
 (1..10).each do |n|
   User.create!(

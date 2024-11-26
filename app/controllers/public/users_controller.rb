9e5c99ebd3eaf4fb83f78a8d0811b36f63515350
EC2 Default User <ec2-user@ip-172-31-4-116.ap-northeast-1.compute.internal>
2024-11-26 19:15:12 +0000

    [update]layout

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,5 +1,5 @@
 class Public::UsersController < ApplicationController
-  before_action :active_for_authentication?, only: [:show, :edit, :update, :destroy]
+  before_action :active_for_authentication?, only: [:show, :edit, :index, :update, :destroy]
   before_action :correct_user, only: [:edit, :update, :destroy]
 
   def show
@@ -26,9 +26,21 @@ class Public::UsersController < ApplicationController
   end
 
   def edit
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to user_path(current_user.id)
+    end
+
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if  @user.update(user_params)
+     redirect_to user_path(params[:id])
+    else
+     render :edit
+    end
   end
 
   def destroy
@@ -59,14 +71,14 @@ class Public::UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image,)
   end
-  
+
   def active_for_authentication?
     @user = User.find_by_id(params[:id])
     if (!current_user || current_user.is_deleted) || (@user.is_deleted)
       redirect_to root_path
     end
   end
-  
+
   def correct_user
     if @user != current_user
       redirect_to lists_path
